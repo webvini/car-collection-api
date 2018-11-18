88dff6c1cbbe4d0a5b5af9aeb5a43cb0b8bbad47
Vinicius Silva <webvini.silva@gmail.com>
2018-11-18 17:58:59 -0200

    Hello World

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const config = require('./config')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(config.port, () => {
+  console.log(`Server listening on port ${config.port}`)
+})
